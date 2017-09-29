14a917369662ef85170c614aa63a331aeb49a557
Jason Dalton <jcdalton@hotmail.com>
2017-09-29 14:09:36 -0500

    test(*): add unit tests

--- test/weather-button-test.js
+++ test/weather-button-test.js
@@ -1,10 +1,17 @@
-
+import '../.temp/weather-button.js';
 describe('Test of Button', ()=>{
-  it('Test we can test', ()=>{
+  let topButton;
+  beforeEach(()=>{
     let Button = document.createElement('div');
     Button.innerHTML = '<weather-button>We have a tooltip now.</weather-button>';
     document.body.appendChild(Button);
-    let topButton = document.querySelector('weather-button');
+    topButton = document.querySelector('weather-button');
+  });
+  it('Test we can test', ()=>{
+    expect(topButton.style['width']).toEqual('54px');
+  });
+  afterEach(()=>{
+    document.body.innerHTML = '';
   });
 
 });
